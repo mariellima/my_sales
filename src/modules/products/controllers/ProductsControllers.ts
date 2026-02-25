e2f2632f772f18e4620dae297d7b7961c4013b90
mariellima <marielgomes1@outlook.com>
2026-02-25 16:51:38 -0300

    feat: controller product

---
+++ src/modules/products/controllers/ProductsControllers.ts
@@ -0,0 +1,53 @@
+import { Response, Request } from "express";
+import ListProductService from "../services/ListProductService";
+import ShowProductService from "../services/ShowProductService";
+import CreateProductService from "../services/CreateProductService";
+import UpdateProductService from "../services/UpdateProductService";
+import DeleteProductService from "../services/DeleteProductService";
+
+
+export default class ProductsControllers {
+  async index(request: Request, response: Response): Promise<Response> {
+    const listProductsService = new ListProductService();
+    const products = await listProductsService.execute();
+    return response.json(products);
+  }
+
+  async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params as { id: string };
+    const showProductService = new ShowProductService();
+    const product = await showProductService.execute({ id });
+    return response.json(product);
+  }
+
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body;
+    const createProductService = new CreateProductService();
+    const product = await createProductService.execute({
+      name,
+      price,
+      quantity,
+    });
+    return response.json(product);
+  }
+
+  async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params as { id: string };
+    const { name, price, quantity } = request.body;
+    const updateProductService = new UpdateProductService();
+    const product = await updateProductService.execute({
+      id,
+      name,
+      price,
+      quantity,
+    });
+    return response.json(product);
+  }
+
+  async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params as { id: string };
+    const deleteProductService = new DeleteProductService();
+    await deleteProductService.execute({ id });
+    return response.status(204).send([]);
+  }
+}
