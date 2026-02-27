3f72b96fef2c2652e5f0e3faeb914fc2ae29cd2b
mariellima <marielgomes1@outlook.com>
2026-02-27 16:26:05 -0300

    feat: create user service

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,30 @@
+import AppError from "@shared/errors/AppError";
+import { usersRepositories } from "../database/repositories/UsersRepositories";
+import { User } from "../database/entities/User";
+import { hash } from "bcrypt";
+
+interface ICreateUser {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default class CreateUserService {
+  async execute({ name, email, password }: ICreateUser): Promise<User> {
+    const emailExists = await usersRepositories.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError("Email address already used.", 409);
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const user = usersRepositories.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
