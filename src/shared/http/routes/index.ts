c98bbb6404aad86f4f68593759f12be2142f1ae0
mariellima <marielgomes1@outlook.com>
2026-03-05 09:06:10 -0300

    feat: ignore

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,7 +2,8 @@ import productsRouter from "@modules/products/routes/ProductsRoutes";
 import avatarRouter from "@modules/users/routes/AvatarRoutes";
 import sessionsRouter from "@modules/users/routes/SessionRoutes";
 import usersRouter from "@modules/users/routes/UserRoutes";
-import { Router } from "express";
+import express, { Router } from "express";
+import uploadConfig from "@config/upload";
 
 const routes = Router();
 
@@ -13,5 +14,6 @@ routes.use("/products", productsRouter);
 routes.use("/users", usersRouter);
 routes.use("/sessions", sessionsRouter);
 routes.use("/avatar", avatarRouter);
+routes.use("/files", express.static(uploadConfig.directory));
 
 export default routes;
