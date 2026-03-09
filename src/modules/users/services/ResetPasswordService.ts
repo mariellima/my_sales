3902429f297b5f842eff1a7cb549f74f3acab1d0
mariellima <marielgomes1@outlook.com>
2026-03-09 16:28:08 -0300

    feat: email config and send

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -17,7 +17,7 @@ export default class ResetPasswordService {
       throw new AppError("User token does not exists.", 404);
     }
 
-    const user = await usersRepositories.findById(userToken.id);
+    const user = await usersRepositories.findById(userToken.user_id);
 
     if (!user) {
       throw new AppError("User does not exists.", 404);
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,7 @@
 import AppError from "@shared/errors/AppError";
 import { usersRepositories } from "../database/repositories/UsersRepositories";
 import { UserTokensRepositories } from "../database/repositories/UserTokensRepositories";
+import { sendEmail } from "@config/email";
 
 interface IForgetPassword {
   email: string;
@@ -16,6 +17,26 @@ export default class SendForgotPasswordEmailService {
 
     const token = await UserTokensRepositories.generate(user.id);
 
-    console.log(token);
+    sendEmail({
+      to: email,
+      subject: "My Sales Recovery Password",
+      body: `
+      <div style="font-family: Arial, sans-serif; padding: 20px; color: #333;
+      textlalign: center; border: 2px solid #041d40; border-radius: 10px;
+      margin: auto; width: 60%;">
+        <h1 style="color: #041d40;">Password Reset Verification Code</h1>
+        <h3 style="color: #041d40;">Dear ${user.name},</h3>
+        <p style="font-size: 16px; color: #333;">Recover your password with this
+        token :< /p>
+        <p>
+          <strong style="border: 2px dashed #041d40; padding: 10px;
+          border-radius: 5px; font-size: 16px; color: #041d40;">${token?.token}</
+          strong>
+        </p>
+        <p style="margin-top: 20px;">Best regards,<br><span style="font-weight:
+        bold; color: #041d40;">My Sales Staff</span></p>
+      </div>
+      `,
+    });
   }
 }
