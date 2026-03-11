57497d1e863629b1c3d5521424566d95e0575ca6
mariellima <marielgomes1@outlook.com>
2026-03-11 15:19:11 -0300

    feat: customer repository

---
+++ src/modules/customers/database/repositories/CustomerRepositories.ts
@@ -0,0 +1,21 @@
+import { AppDataSource } from "@shared/typeorm/data-source";
+import { Customer } from "../entities/Customer";
+
+export const customerRepositories = AppDataSource.getRepository(
+  Customer,
+).extend({
+  async findByName(name: string): Promise<Customer | null> {
+    const customer = await this.findOneBy({ name });
+    return customer;
+  },
+
+  async findbyId(id: number): Promise<Customer | null> {
+    const customer = await this.findOneBy({ id });
+    return customer;
+  },
+
+  async findByEmail(email: string): Promise<Customer | null> {
+    const customer = await this.findOneBy({ email });
+    return customer;
+  },
+});
