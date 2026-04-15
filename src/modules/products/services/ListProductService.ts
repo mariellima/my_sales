5a5b858aa472a53491f1b0508b74b4fd3d22196d
mariellima <marielgomes1@outlook.com>
2026-04-14 22:28:01 -0300

    feat: integrate Redis caching for product services and add Redis configuration

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,10 +1,26 @@
+import RedisCache from "@shared/cache/RedisCache";
 import { Product } from "../database/entities/Product";
 import { productsRepositories } from "../database/repositories/ProductsRepositories";
 
 export default class ListProductService {
   async execute(): Promise<Product[]> {
-    const products = await productsRepositories.find();
+    const rediscache = new RedisCache();
 
+    let products = await rediscache.recover<Product[]>(
+      "api-mysales-PRODUCT_LIST",
+    );
+
+    if (!products) {
+      products = await productsRepositories.find();
+
+      await rediscache.save(
+        "api-mysales-PRODUCT_LIST",
+        JSON.stringify(products),
+      );
+      return products;
+    }
+
+    // const products = await productsRepositories.find();
     return products;
   }
 }
